de9fdb039c74a57315b67f4c0fcc7e0999319869
EMS engineering <j39g8bxh@gmail.com>
2022-10-15 13:11:35 +0900

    情報表示用コンポーネントの作成 #31

---
+++ frontend/src/components/common/DisplayInformation.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import router from '@/router'
+
+interface Props {
+    title?: string,
+    content?: string,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    title: '',
+    content: '',
+});
+
+const toHomePage = () => {
+    router.push('/home');
+}
+    
+</script>
+
+<template>
+    <div class="information-row">
+        <div class="information-title">
+            <span>{{props.title}}</span>
+        </div>
+        <div class="information-content">
+            <span>{{props.content}}</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.information-row {
+    display: flex;
+    border-top: 1px  solid #33333380;
+    border-bottom: 1px  solid #33333380;
+}
+.information-title {
+    width: 40%;
+    min-height: 30px;
+    height: auto;
+    display: table;
+    background-color: #eee;
+    border-right: 1px solid #33333380;
+}
+.information-content {
+    width: 60%;
+    height: 30px;
+    display: table;
+}
+span {
+    display: table-cell;
+    vertical-align: middle;
+}
+</style>
