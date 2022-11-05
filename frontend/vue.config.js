171951b1df90134d3bc90563535e294a90c140e7
EMS engineering <j39g8bxh@gmail.com>
2022-11-05 17:46:01 +0900

    vue.config.jsにプロキシの設定を追加 #35

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+module.exports = {
+  devServer: {
+    port: 8080,
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8082',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '/' },
+      },
+    },
+  },
+};
