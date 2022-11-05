47c918352f63f03b12ee153a9f106c05ce752f08
EMS engineering <j39g8bxh@gmail.com>
2022-11-05 17:37:14 +0900

    新規登録serviceの作成 #35 controllerからuserDataを受け取り，そのデータを基にDBに追加する処理を追加

---
+++ backend/src/main/java/com/example/stamp_app/service/RegisterService.java
@@ -0,0 +1,60 @@
+package com.example.stamp_app.service;
+
+import com.example.stamp_app.entity.Account;
+import com.example.stamp_app.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class RegisterService {
+    @Autowired
+    AccountRepository accountRepository;
+
+    /**
+     * アカウント追加Service
+     *
+     * @param userData 登録情報
+     * @return HttpStatus
+     */
+    public HttpStatus addAccount(Account userData) {
+
+        boolean isNewUser;
+
+        if (userData == null) {
+            return HttpStatus.BAD_REQUEST;
+        }
+
+        try {
+            isNewUser = accountRepository.findByEmail(userData.getEmail()) != null;
+        } catch (Exception exception) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        if (!isNewUser) {
+            System.out.println(" The email address has already been used.");
+            return HttpStatus.CONFLICT;
+        }
+
+        String hashedPassword = DigestUtils.md5DigestAsHex(userData.getPassword().getBytes());
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Account newUser = new Account();
+        newUser.setUlid(UUID.randomUUID());
+        newUser.setEmail(userData.getEmail());
+        newUser.setPassword(hashedPassword);
+        newUser.setCreatedAt(localDateTime);
+
+        try {
+            accountRepository.save(newUser);
+        } catch (Exception exception) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        System.out.println("Successfully registered.");
+        return HttpStatus.OK;
+    }
+}
