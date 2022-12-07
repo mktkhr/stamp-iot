656cfa21fb72e8e8e3d080a514bb3fb494c94711
EMS engineering <j39g8bxh@gmail.com>
2022-12-08 00:11:00 +0900

    テスト用にダミーデータクラスを作成 #40

---
+++ backend/src/test/java/com/example/stamp_app/dummyData/DummyData.java
@@ -0,0 +1,25 @@
+package com.example.stamp_app.dummyData;
+
+public class DummyData {
+    public static final String REGISTER_POST_PATH = "/ems/account/register";
+
+    public static final String INVALID_7_LENGTH_PASSWORD = "A123456";
+
+    public static final String VALID_8_LENGTH_PASSWORD = "A1234567";
+
+    public static final String INVALID_8_LENGTH_PASSWORD = "a1234567";
+
+    public static final String VALID_9_LENGTH_PASSWORD = "A12345678";
+
+    public static final String VALID_23_LENGTH_PASSWORD = "A1234567890123456789012";
+
+    public static final String VALID_24_LENGTH_PASSWORD = "A12345678901234567890123";
+
+    public static final String INVALID_24_LENGTH_PASSWORD = "a12345678901234567890123";
+
+    public static final String INVALID_25_LENGTH_PASSWORD = "A123456789012345678901234";
+
+    public static final String VALID_EMAIL_ADDRESS = "aaa@example.com";
+
+    public static final String INVALID_EMAIL_ADDRESS = "aaaexample.com";
+}
