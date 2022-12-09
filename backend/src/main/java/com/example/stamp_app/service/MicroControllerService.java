4d842e8566aee2fb20b567a96766d36217fc0e0f
EMS engineering <j39g8bxh@gmail.com>
2022-12-09 21:49:31 +0900

    マイクロコントローラーコントローラーがエラー時にexceptionを投げるように修正 #40

--- backend/src/main/java/com/example/stamp_app/service/MicroControllerService.java
+++ backend/src/main/java/com/example/stamp_app/service/MicroControllerService.java
@@ -8,6 +8,7 @@ import com.example.stamp_app.repository.MicroControllerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigInteger;
 
@@ -24,49 +25,45 @@ public class MicroControllerService {
         MicroController microController;
         Account requestedAccount;
 
-        try{
+        try {
             // 対象のMAC Addressのマイクロコントローラー情報を取得
             microController = microControllerRepository.findByMacAddress(macAddress);
-        }catch (Exception e){
-            System.out.println(e);
-            return new MicroControllerPostResponse(HttpStatus.INTERNAL_SERVER_ERROR, null);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
 
         // 対象のマイクロコントローラーがDB上に存在しなかった場合，400を返す
         if(microController == null){
-            return new MicroControllerPostResponse(HttpStatus.BAD_REQUEST, null);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
         try {
             // リクエストしたアカウント情報を取得
             requestedAccount = accountRepository.findById(userId);
-        }catch(Exception e){
-            System.out.println(e);
-            return new MicroControllerPostResponse(HttpStatus.INTERNAL_SERVER_ERROR, null);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         // 対象のアカウントが存在しなかった場合，400を返す
-        if(requestedAccount == null){
-            return new MicroControllerPostResponse(HttpStatus.BAD_REQUEST, null);
+        if (requestedAccount == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
         // 既にマイクロコントローラーがアカウントに紐づけられていた場合，401を返す
-        if(microController.getAccount() != null){
-            return new MicroControllerPostResponse(HttpStatus.UNAUTHORIZED, null);
+        if (microController.getAccount() != null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
         }
 
         microController.setAccount(requestedAccount);
 
-        try{
+        try {
             // マイクロコントローラー情報を更新する
             microControllerRepository.save(microController);
-        }catch(Exception e) {
-            System.out.println(e);
-            return new MicroControllerPostResponse(HttpStatus.INTERNAL_SERVER_ERROR, null);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        return new MicroControllerPostResponse(HttpStatus.UNAUTHORIZED, microController);
+        return new MicroControllerPostResponse(HttpStatus.OK, microController);
 
     }
 }
