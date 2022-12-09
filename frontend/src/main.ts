570ab5907a5242d646cf0aa4497f0e074cb935eb
EMS engineering <j39g8bxh@gmail.com>
2022-12-09 21:47:03 +0900

    appがvuetifyを使用するようにmain.tsに追加 #40

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,16 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import { createPinia } from 'pinia';
+import { createVuetify } from 'vuetify';
 import '@/style/common-style.css';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
 const app = createApp(App);
 
 const pinia = createPinia();
+const vuetify = createVuetify({ components, directives });
 app.use(pinia);
+app.use(vuetify);
 app.use(router);
 app.mount('#app');
