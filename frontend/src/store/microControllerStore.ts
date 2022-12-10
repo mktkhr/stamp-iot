b4c7972c934eee46ba0823e6070976ffeef2950f
EMS engineering <j39g8bxh@gmail.com>
2022-12-10 12:13:23 +0900

    マイコンStoreを作成 #45

---
+++ frontend/src/store/microControllerStore.ts
@@ -0,0 +1,26 @@
+import { getMicroControllerInfo, MicroControllerInfoState } from '@/type/account/microController';
+import { defineStore } from 'pinia';
+
+export const MicroControllerStore = defineStore('MicroControllerStore', {
+  state: () => ({
+    microControllerList: new Array<MicroControllerInfoState>(),
+  }),
+  getters: {
+    getMicroControllerList: (state): Array<MicroControllerInfoState> => {
+      return state.microControllerList;
+    },
+  },
+  actions: {
+    /**
+     * Cookieの情報を基にredisからアカウントUUIDを取得し，アカウントUUIDからマイコンリストをもらってstoreに保存
+     */
+    async fetchAccountInfo() {
+      let microControllerList: Array<MicroControllerInfoState>;
+      try {
+        microControllerList = await getMicroControllerInfo();
+      } finally {
+        this.$state.microControllerList = microControllerList;
+      }
+    },
+  },
+});
