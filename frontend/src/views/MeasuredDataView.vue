67763adfbce8f7d8f6ea527b593601e0c3500c96
EMS engineering <j39g8bxh@gmail.com>
2022-12-22 20:56:28 +0900

    環境データとSDI-12関連データのグラフを選択肢から表示を切り替えられるように実装 #47

--- frontend/src/views/MeasuredDataView.vue
+++ frontend/src/views/MeasuredDataView.vue
@@ -2,11 +2,18 @@
 import HeaderComponent from '@/components/HeaderComponent.vue';
 import { computed, ref, watch } from 'vue';
 import NavigatorComponent from '@/components/NavigatorComponent.vue';
+import InformationSelect from '@/components/common/InformationSelect.vue';
 import { MicroControllerStore } from '@/store/microControllerStore';
 import { MeasuredDataStore } from '@/store/measuredDataStore';
 import { useRoute } from 'vue-router';
 import { LineChart } from 'vue-chart-3';
 import { Chart, ChartData, ChartOptions, registerables } from 'chart.js';
+import {
+  convertSdi12KeyWordToTitle,
+  convertSdi12KeyWordToScale,
+  convertEnvironmentalKeyWordToTitle,
+  convertEnvironmentalKeyWordToScale,
+} from '@/type/measuredData';
 
 // Route
 const route = useRoute();
@@ -24,27 +31,29 @@ const changeState = (param: boolean) => {
   menuStateRef.value = param;
 };
 
+// 初期値
+const selectedSdi12Option = ref('');
+const selectedEnvironmentalOption = ref('');
+
 // Chart関連
 Chart.register(...registerables);
 
-const config = computed<ChartOptions<'line'>>(() => ({
+
+// SDI-12グラフの設定
+const sdi12ChartConfig = computed<ChartOptions<'line'>>(() => ({
   responsive: true,
   scales: {
     x: {
-      // 軸設定(https://www.chartjs.org/docs/latest/axes/)
-      type: 'linear', // 軸のタイプ指定(詳細不明だが，linearでないと全データが等間隔で表示される)
-      display: true, // 表示の有無
-      position: 'bottom', // 軸表示位置 top or bottom
+      type: 'linear',
+      display: true,
+      position: 'bottom',
       title: {
-        display: true, // 軸タイトル表示有無
-        align: 'center', // 軸タイトル表示位置 start or center or end
-        text: 'DOY', // 軸タイトル
+        display: true,
+        align: 'center',
+        text: 'DOY',
         font: {
-          size: 14, // 軸タイトルフォントサイズ
+          size: 14,
         },
-      },
-      ticks: {
-        // stepSize: 0.01, // 軸ラベル間隔
       },
     },
     y: {
@@ -54,33 +63,120 @@ const config = computed<ChartOptions<'line'>>(() => ({
       title: {
         display: true,
         align: 'center',
-        text: 'Y軸タイトル',
+        text: convertSdi12KeyWordToScale(selectedSdi12Option.value),
         font: {
           size: 14,
         },
-      },
-      ticks: {
-        // stepSize: 0.1,
       },
     },
   },
   plugins: {
     legend: {
-      display: true, // 凡例表示有無
+      display: true,
     },
     title: {
-      text: 'グラフタイトル', // グラフタイトル
-      display: true, // グラフタイトル表示有無
+      text: convertSdi12KeyWordToTitle(selectedSdi12Option.value),
+      display: true,
       font: {
-        size: 16, // グラフタイトルフォントサイズ
+        size: 16,
       },
     },
   },
 }));
 
-const dataset = computed(() => measuredDataStore.getSdi12DataList('soilTemp')); // ここをセレクト要素で分岐させる
-const lineData = computed<ChartData<'line'>>(() => ({
-  datasets: dataset.value,
+// 環境データグラフの設定
+const environmentalChartConfig = computed<ChartOptions<'line'>>(() => ({
+  responsive: true,
+  scales: {
+    x: {
+      type: 'linear',
+      display: true,
+      position: 'bottom',
+      title: {
+        display: true,
+        align: 'center',
+        text: 'DOY',
+        font: {
+          size: 14,
+        },
+      },
+    },
+    y: {
+      type: 'linear',
+      display: true,
+      position: 'bottom',
+      title: {
+        display: true,
+        align: 'center',
+        text: convertEnvironmentalKeyWordToScale(selectedEnvironmentalOption.value),
+        font: {
+          size: 14,
+        },
+      },
+    },
+  },
+  plugins: {
+    legend: {
+      display: true,
+    },
+    title: {
+      text: convertEnvironmentalKeyWordToTitle(selectedEnvironmentalOption.value),
+      display: true,
+      font: {
+        size: 16,
+      },
+    },
+  },
+}));
+
+// SDI-12グラフ表示切替用選択肢
+const sdi12OptionList = [
+  { word: '体積含水率', key: 'vwc' },
+  { word: 'バルク比誘電率', key: 'brp' },
+  { word: '地温', key: 'soilTemp' },
+  { word: 'バルク電気伝導度', key: 'sbec' },
+  { word: '土壌間隙水電気伝導度', key: 'spwec' },
+  { word: '重力加速度(X)', key: 'gax' },
+  { word: '重力加速度(Y)', key: 'gay' },
+  { word: '重力加速度(Z)', key: 'gaz' },
+];
+
+// 環境データグラフ表示切替用選択肢
+const environmentalOptionList = [
+  { word: '大気圧', key: 'airPress' },
+  { word: '気温', key: 'temp' },
+  { word: '相対湿度', key: 'humi' },
+  { word: '二酸化炭素濃度', key: 'co2Concent' },
+  { word: '総揮発性有機化合物量', key: 'tvoc' },
+  { word: 'アナログ値', key: 'analogValue' },
+];
+
+/**
+ * SDI-12の表示項目セレクターが変化した際の処理
+ * @param value
+ */
+const onChangeSdi12Select = (value: string) => {
+  selectedSdi12Option.value = value;
+};
+
+// SDI-12グラフ用データ
+const sdi12ChartData = computed(() => measuredDataStore.getSdi12DataList(selectedSdi12Option.value));
+const sdi12ChartDataSet = computed<ChartData<'line'>>(() => ({
+  datasets: sdi12ChartData.value,
+}));
+
+/**
+ * SDI-12の表示項目セレクターが変化した際の処理
+ * @param value
+ */
+const onChangeEnvironmentalSelect = (value: string) => {
+  selectedEnvironmentalOption.value = value;
+};
+
+// 環境データグラフ用データ
+const environmentalChartData = computed(() => measuredDataStore.getEnvironmentDataList(selectedEnvironmentalOption.value));
+const environmentalChartDataSet = computed<ChartData<'line'>>(() => ({
+  datasets: environmentalChartData.value,
 }));
 </script>
 
@@ -89,7 +185,24 @@ const lineData = computed<ChartData<'line'>>(() => ({
   <NavigatorComponent :menuState="menuStateRef" />
   <div class="main-content">
     <div class="graph-wrapper">
-      <LineChart ref="lineRef" :chartData="lineData" :options="config" />
+      <LineChart ref="lineRef" :chartData="sdi12ChartDataSet" :options="sdi12ChartConfig" />
+      <InformationSelect
+        title="表示項目"
+        :option-list="sdi12OptionList"
+        @selectedValue="onChangeSdi12Select"
+      />
+    </div>
+    <div class="graph-wrapper">
+      <LineChart
+        ref="lineRef"
+        :chartData="environmentalChartDataSet"
+        :options="environmentalChartConfig"
+      />
+      <InformationSelect
+        title="表示項目"
+        :option-list="environmentalOptionList"
+        @selectedValue="onChangeEnvironmentalSelect"
+      />
     </div>
   </div>
 </template>
