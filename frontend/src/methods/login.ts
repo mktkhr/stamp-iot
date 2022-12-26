2aef09044338ccd2fc1ebdab60fd2572918c3f47
EMS engineering <j39g8bxh@gmail.com>
2022-12-26 22:03:33 +0900

    ログインのエラーメッセージをバックエンドに合わせて修正 #50

--- frontend/src/methods/login.ts
+++ frontend/src/methods/login.ts
@@ -12,12 +12,12 @@ export default {
         router.replace('/home');
       })
       .catch((error) => {
-        if (error.response.status == '409') {
-          alert('このメールアドレスは既に使用されています。');
+        if (error.response.status == '401') {
+          alert('アカウント情報が間違っています。');
         } else if (error.response.status == '500') {
           alert('エラーが発生しました。時間をおいて再度お試しください。');
         } else if (error.response.status == '400') {
-          alert('エラーが発生しました。再度入力してください。');
+          alert('アカウント情報が間違っています。');
         } else {
           alert('予期せぬエラーが発生しました。時間をおいて再度お試しください。');
         }
