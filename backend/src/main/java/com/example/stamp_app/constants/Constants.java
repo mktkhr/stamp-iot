f83988667bfb946ce333143cf6eaffce02ebd3df
EMS engineering <j39g8bxh@gmail.com>
2022-12-26 21:59:27 +0900

    セッションの有効j期限を1日に延長 #50

--- backend/src/main/java/com/example/stamp_app/constants/Constants.java
+++ backend/src/main/java/com/example/stamp_app/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.example.stamp_app.constants;
 
 public class Constants {
-    public static final int SESSION_VALID_TIME_IN_SEC = 3600;
+    public static final int SESSION_VALID_TIME_IN_SEC = 60 * 60 * 24;
     public static final String COOKIE_NAME = "ems_session";
 }
