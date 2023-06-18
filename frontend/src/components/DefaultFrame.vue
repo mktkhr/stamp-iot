37ef106809091ba5455de4647cc12188aeaee3d5
EMS engineering <j39g8bxh@gmail.com>
2023-06-18 18:50:15 +0900

    [add] #79 共通で使用するフレームコンポーネントを追加

---
+++ frontend/src/components/DefaultFrame.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+interface Props {
+  showActionBar?: boolean;
+  showBackButton?: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+  showActionBar: false,
+  showBackButton: false,
+});
+</script>
+<template>
+  <div class="wrapper">
+    <div class="wrapper-action-bar" v-show="showActionBar">
+      <slot name="actionBar"></slot>
+    </div>
+    <div
+      :class="{
+        'wrapper-main-content': showActionBar,
+        'wrapper-main-content-no-action-bar': !showActionBar,
+      }"
+    >
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$action_bar_height: 50px;
+.wrapper {
+  height: 100%;
+  width: 100%;
+  &-action-bar {
+    height: #{$action_bar_height};
+    width: 100%;
+  }
+  &-main-content {
+    height: calc(100% - #{$action_bar_height});
+    width: 100%;
+    &-no-action-bar {
+      height: 100%;
+      width: 100%;
+    }
+  }
+}
+</style>
