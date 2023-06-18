90a971e3547a35b53a6c00ed21d93adf098d0035
EMS engineering <j39g8bxh@gmail.com>
2023-06-18 18:50:53 +0900

    [add] #79 グリッド表示用共通コンポーネントを追加

---
+++ frontend/src/components/common/GridFrame.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import Plus from 'vue-material-design-icons/Plus.vue';
+
+interface Props {
+  title?: string;
+  isForMultiMedia?: boolean;
+  gap?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  title: '',
+  isForMultiMedia: true,
+  gap: '0px',
+});
+</script>
+
+<template>
+  <div
+    class="wrapper-grid"
+    :class="{
+      'grid-one-half': isForMultiMedia,
+      'grid-one-fourth': isForMultiMedia,
+    }"
+    :style="{ gap: `${gap}` }"
+  >
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper-grid {
+  height: auto;
+  width: 100%;
+  overflow-y: scroll;
+  display: grid;
+  grid-template-columns: 1fr;
+}
+
+@media (min-width: 800px) {
+  .grid-one-half {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media (min-width: 1200px) {
+  .grid-one-fourth {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+</style>
