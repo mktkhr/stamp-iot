8eb8634f99ea6572f7dbea7599ba4564c2bef1c6
EMS engineering <j39g8bxh@gmail.com>
2023-07-15 23:42:00 +0900

    [add] #87 アカウントコントローラーにspringdocを追加

--- backend/src/main/java/com/example/stamp_app/controller/param/account/LoginPostParam.java
+++ backend/src/main/java/com/example/stamp_app/controller/param/account/LoginPostParam.java
@@ -1,18 +1,22 @@
 package com.example.stamp_app.controller.param.account;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(description = "ログインパラメータ")
 public class LoginPostParam {
 
-    @Pattern(regexp = "^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$")
     @NotBlank
+    @Schema(description = "メールアドレス", example = "aaa@example.com")
+    @Pattern(regexp = "^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$")
     private String email;
 
     // NOTE: ログイン時のパスワードはパターン推測を回避するため，パターンバリデーションを行わない
     @NotBlank
+    @Schema(description = "パスワード", example = "SamplePassword")
     private String password;
 
 }
