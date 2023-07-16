cdd34c3b601ec244d388810c2beeacb916276c04
EMS engineering <j39g8bxh@gmail.com>
2023-07-16 12:35:17 +0900

    [update] #87 dev環境かつspringdoc用のエンドポイントへのアクセスの場合はtrueを返すように修正

--- backend/src/main/java/com/example/stamp_app/config/AppInterceptor.java
+++ backend/src/main/java/com/example/stamp_app/config/AppInterceptor.java
@@ -8,9 +8,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Objects;
 
 @Slf4j
 public class AppInterceptor implements HandlerInterceptor {
@@ -21,6 +24,8 @@ public class AppInterceptor implements HandlerInterceptor {
     RedisService redisService;
     @Autowired
     RequestedUser requestedUser;
+    @Value("${spring.profiles.active}")
+    String activeProfile;
 
     /**
      * Cookieを基に，セッションの有無を確認する
@@ -29,6 +34,11 @@ public class AppInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws ResponseStatusException {
 
         var path = request.getRequestURI();
+
+        // アクティブプロファイルがdevかつspringdoc用エンドポイントへのアクセスの場合
+        if (Objects.equals(activeProfile, "dev") && (path.contains("/swagger-ui") || path.contains("/api-docs"))) {
+            return true;
+        }
 
         // セッションの有無を確認しないリクエスト
         if (path.contains("/login") || path.contains("/register") || path.contains("/session")) {
