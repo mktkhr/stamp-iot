37806153be13f14f0c38ef62416c6f477cdb9c97
EMS engineering <j39g8bxh@gmail.com>
2023-08-06 01:13:45 +0900

    [add] #98 マイコン設定値を取得する処理を追加

--- MainCode/MainCode.ino
+++ MainCode/MainCode.ino
@@ -1042,6 +1042,204 @@ void postRequest(String host, String uri, String measureData)
 }
 
 /**
+ * @brief Getリクエストを送信
+ *
+ */
+void getRequest()
+{
+  Serial.println("\r\n-----Connecting to HOST-----\r\n");
+  String targetHost;
+  int targetPort;
+  WiFiClientSecure sslClient;
+  WiFiClient client;
+  // ArduinoJson
+  const int BUFFER_SIZE = JSON_OBJECT_SIZE(4) + JSON_ARRAY_SIZE(1);
+  StaticJsonDocument<BUFFER_SIZE> jsonBuffer;
+
+  const String bootMode = readPreference(0);
+  if (bootMode == String(NORMAL_MODE))
+  {
+    targetHost = emsHost;
+    targetPort = productionPort;
+    sslClient.setInsecure();
+    sslClient.setCACert(PUBLIC_KEY);
+  }
+  else
+  {
+    targetHost = readPreference(3);
+    targetPort = localPort;
+  }
+
+  Serial.print("TargetHost: ");
+  Serial.println(targetHost);
+  Serial.print("TargetPort: ");
+  Serial.println(targetPort);
+
+  // ホスト名(String)をchar*に変換
+  int length = targetHost.length() + 1;
+  char bufferArray[length];
+  targetHost.toCharArray(bufferArray, length);
+  const char *hostCharPointer = bufferArray;
+
+  const int requestLimit = 5;
+
+  for (int j = 0; j < requestLimit; j++)
+  {
+    if (bootMode == String(NORMAL_MODE))
+    {
+      if (sslClient.connect(hostCharPointer, targetPort))
+      {
+        // HTTPSの場合
+        Serial.println("\r\n-----Get measuring settings-----\r\n");
+        sslClient.println("GET " + getMicroControllerUri + "?macAddress=" + WiFi.macAddress() + " HTTP/1.1");
+        sslClient.println("Host: " + targetHost + ":" + String(targetPort));
+        sslClient.println("Connection: close");
+        sslClient.println("Content-Type: application/json");
+        sslClient.print("Content-Length: 0");
+        sslClient.println();
+        sslClient.println();
+        delay(100);
+
+        // ヘッダーの読み飛ばし
+        String response;
+        while (sslClient.available())
+        {
+          response = sslClient.readStringUntil('\r');
+          response.trim();
+          if (response.length() == 0)
+          {
+            break;
+          }
+        }
+        delay(100);
+
+        // レスポンスボディを取得
+        String responseBuffer;
+        while (sslClient.available())
+        {
+          response = sslClient.readStringUntil('\r');
+          response.trim();
+          responseBuffer.concat(response);
+        }
+
+        // JSONパース
+        char json[responseBuffer.length() + 1];
+        responseBuffer.toCharArray(json, sizeof(json));
+        Serial.println(json);
+        auto error = deserializeJson(jsonBuffer, json);
+        delay(200);
+        if (error)
+        {
+          Serial.print("JSON parse error: ");
+          Serial.println(error.c_str());
+          return;
+        }
+        const char *interval = jsonBuffer["interval"];
+        Serial.println(interval);
+        const char *sdi12Address = jsonBuffer["sdi12Address"];
+        Serial.println(sdi12Address);
+
+        String intervalString = String(interval);
+        String sdi12AddressString = String(sdi12Address);
+        if (intervalString == "1" || intervalString == "5" || intervalString == "10" || intervalString == "15" || intervalString == "20" || intervalString == "30" || intervalString == "60")
+        {
+          writePreference(4, intervalString);
+        }
+
+        writePreference(5, sdi12AddressString);
+
+        Serial.println("closing connection");
+
+        delay(100);
+        sslClient.stop();
+        return;
+      }
+      else
+      {
+        Serial.println(".");
+        delay(200);
+      }
+    }
+    else
+    {
+      // HTTPの場合
+      if (client.connect(hostCharPointer, targetPort))
+      {
+        Serial.println("\r\n-----Get measuring settings-----\r\n");
+        client.println("GET " + getMicroControllerUri + "?macAddress=" + WiFi.macAddress() + " HTTP/1.1");
+        client.println("Host: " + targetHost + ":" + String(targetPort));
+        client.println("Connection: close");
+        client.println("Content-Type: application/json");
+        client.print("Content-Length: 0");
+        client.println();
+        client.println();
+        delay(100);
+
+        // ヘッダーの読み飛ばし
+        String response;
+        while (client.available())
+        {
+          response = client.readStringUntil('\r');
+          Serial.print(response);
+          response.trim();
+          if (response.length() == 0)
+          {
+            break;
+          }
+        }
+        delay(100);
+
+        // レスポンスボディを取得
+        String responseBuffer;
+        while (client.available())
+        {
+          response = client.readStringUntil('\r');
+          response.trim();
+          responseBuffer.concat(response);
+        }
+
+        // JSONパース
+        char json[responseBuffer.length() + 1];
+        responseBuffer.toCharArray(json, sizeof(json));
+        Serial.println(json);
+        auto error = deserializeJson(jsonBuffer, json);
+        delay(200);
+        if (error && responseBuffer.length() == 0)
+        {
+          Serial.print("JSON parse error: ");
+          Serial.println(error.c_str());
+          return;
+        }
+        const char *interval = jsonBuffer["interval"];
+        Serial.println(interval);
+        const char *sdi12Address = jsonBuffer["sdi12Address"];
+        Serial.println(sdi12Address);
+
+        String intervalString = String(interval);
+        String sdi12AddressString = String(sdi12Address);
+        if (intervalString == "1" || intervalString == "5" || intervalString == "10" || intervalString == "15" || intervalString == "20" || intervalString == "30" || intervalString == "60")
+        {
+          writePreference(4, intervalString);
+        }
+
+        writePreference(5, sdi12AddressString);
+
+        Serial.println("closing connection");
+        delay(100);
+        client.stop();
+        return;
+      }
+      else
+      {
+        Serial.println(".");
+        delay(200);
+      }
+    }
+  }
+  Serial.println("\r\n-----GET request failed.-----\r\n");
+}
+
+/**
  * @brief Preferenceに設定値を書き込む
  *
  * @param target 書き込み先(0: 起動モード, 1: ssid, 2: pass, 3: host, 4: 測定間隔, 5: 測定アドレス)
