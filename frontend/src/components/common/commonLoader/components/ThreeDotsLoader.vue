398ea73a62c20dc04bddf1e7eb82fbeeffcd098f
EMS engineering <j39g8bxh@gmail.com>
2024-07-29 00:28:53 +0900

    [add] #162 3点Loaderを追加

---
+++ frontend/src/components/common/commonLoader/components/ThreeDotsLoader.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+interface Props {
+  size?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  size: '15px',
+});
+</script>
+
+<template>
+  <div class="hollow-dots-spinner">
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$dot_size: v-bind(size);
+
+.hollow-dots-spinner,
+.hollow-dots-spinner * {
+  box-sizing: border-box;
+}
+
+.hollow-dots-spinner {
+  height: $dot_size;
+  width: calc($dot_size * 2 * 3);
+}
+
+.hollow-dots-spinner .dot {
+  width: $dot_size;
+  height: $dot_size;
+  margin: 0 calc($dot_size / 2);
+  border: calc($dot_size / 5) solid var(--ems-theme);
+  border-radius: 50%;
+  float: left;
+  transform: scale(0);
+  animation: hollow-dots-spinner-animation 1000ms ease infinite 0ms;
+}
+
+.hollow-dots-spinner .dot:nth-child(1) {
+  animation-delay: calc(300ms * 1);
+}
+
+.hollow-dots-spinner .dot:nth-child(2) {
+  animation-delay: calc(300ms * 2);
+}
+
+.hollow-dots-spinner .dot:nth-child(3) {
+  animation-delay: calc(300ms * 3);
+}
+
+@keyframes hollow-dots-spinner-animation {
+  50% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
