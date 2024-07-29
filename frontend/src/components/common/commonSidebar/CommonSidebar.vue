cbae15f3601dfabd45936f798794693056ea5852
EMS engineering <j39g8bxh@gmail.com>
2024-07-30 00:02:13 +0900

    [add] #162 サイドバーコンポーネントを追加

---
+++ frontend/src/components/common/commonSidebar/CommonSidebar.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import SidebarTile from './components/sidebarTile/SidebarTile.vue';
+</script>
+
+<template>
+  <div class="wrapper-sidebar">
+    <RouterLink to="/home">
+      <SidebarTile :title="$t('Navigator.home')">
+        <template #icon>
+          <span class="ems-home common-icon-style"></span>
+        </template>
+      </SidebarTile>
+    </RouterLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  &-sidebar {
+    height: 100%;
+    width: 100%;
+    padding: 8px;
+    display: flex;
+    flex-direction: column;
+    transition: 0.4s ease;
+    position: relative;
+  }
+}
+.current {
+  background-color: rgba(var(--ems-theme));
+}
+</style>
