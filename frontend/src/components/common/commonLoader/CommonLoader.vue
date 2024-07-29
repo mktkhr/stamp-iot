cdfcb59cf9a5eea5fe1f26e54063d7895110254c
EMS engineering <j39g8bxh@gmail.com>
2024-07-30 00:00:40 +0900

    [add] #162 スピナーコンポーネントを追加

---
+++ frontend/src/components/common/commonLoader/CommonLoader.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import CircleLoader from './components/CircleLoader.vue';
+import FlowerLoader from './components/FlowerLoader.vue';
+import ThreeDotsLoader from './components/ThreeDotsLoader.vue';
+import { LOADER_TYPE, LoaderType, useCommonLoader } from './composable';
+
+interface Props {
+  pattern: LoaderType;
+}
+
+defineProps<Props>();
+
+useCommonLoader();
+</script>
+
+<template>
+  <CircleLoader v-if="pattern.id === LOADER_TYPE.CIRCLE.id" />
+  <FlowerLoader v-else-if="pattern.id === LOADER_TYPE.FLOWER.id" />
+  <ThreeDotsLoader v-else />
+</template>
+
+<style lang="scss" scoped></style>
