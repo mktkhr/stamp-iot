2939abf42872cd53f9faf976826f03d603bdb28b
EMS engineering <j39g8bxh@gmail.com>
2024-08-14 00:17:03 +0900

    [update] #162 メニューのindex指定を追加

--- frontend/src/config/zindex.ts
+++ frontend/src/config/zindex.ts
@@ -2,3 +2,5 @@ export const HEADER_Z_INDEX = 1000;
 export const SIDEBAR_Z_INDEX = 100;
 export const OVERLAY_CONTENT_Z_IDNEX = 2000;
 export const OVERLAY_Z_INDEX = 2001;
+export const MENU_CONTENT_Z_INDEX = 1900;
+export const MENU_Z_INDEX = 1901;
