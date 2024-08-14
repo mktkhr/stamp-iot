563e840153ecbfbb25286ef0780e8d0d5f1cca60
EMS engineering <j39g8bxh@gmail.com>
2024-08-15 00:05:10 +0900

    [update] #162 アカウント削除ダイアログの説明文の文字列リソースを追加

--- frontend/src/locales/common/dialog/en.ts
+++ frontend/src/locales/common/dialog/en.ts
@@ -3,6 +3,7 @@ export const Dialog = {
   cancelConfirmDescription: 'Edits are not saved.\nAre you sure?',
   accountDeleteConfirm: 'Account Delete Confirm',
   checkFollowingItems: 'Please check the following items.',
+  accountDeleteConfirmCheck: 'Your account will be deleted. Please confirm the two notes below.',
   accountDeleteConfirmDescription: 'Your account will be deleted. Are you sure?',
   relatedDataDeleteConfirmDescription:
     'The data associated with the account will be deleted. Are you sure?',
