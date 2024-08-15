2731b2fb6ad803602c8d0d36f49847eb5f201b39
EMS engineering <j39g8bxh@gmail.com>
2024-08-15 21:33:36 +0900

    [add] #162 アラートグループのコンポーネントを追加

---
+++ frontend/src/components/common/commonAlertGroup/CommonAlertGroup.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { AlertStore } from '@/store/alertStore';
+import { computed } from 'vue';
+import CommonAlert from '../commonAlert/CommonAlert.vue';
+
+const alertStore = AlertStore();
+const alertList = computed(() => alertStore.getAlertList);
+</script>
+
+<template>
+  <div v-if="alertList.length > 0" class="wrapper-alert-box">
+    <CommonAlert v-for="alert in alertList" :key="alert.id" :alert="alert" />
+  </div>
+</template>
+
+<style lang="scss">
+.wrapper {
+  &-alert-box {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+}
+</style>
