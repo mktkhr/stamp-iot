c9647cf79fccfe99c7189618354360d4d84c8272
EMS engineering <j39g8bxh@gmail.com>
2024-08-15 16:44:41 +0900

    [update] #162 アラートのzindexを追加

--- frontend/src/config/zindex.ts
+++ frontend/src/config/zindex.ts
@@ -4,3 +4,4 @@ export const OVERLAY_CONTENT_Z_IDNEX = 2000;
 export const OVERLAY_Z_INDEX = 2001;
 export const MENU_CONTENT_Z_INDEX = 1900;
 export const MENU_Z_INDEX = 1901;
+export const ALERT_Z_INDEX = 1950;
