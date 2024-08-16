93f19352127c45478fadedec33435f484c35a94c
EMS engineering <j39g8bxh@gmail.com>
2024-08-17 00:32:47 +0900

    [add] #162 フォームのコンポーネントを追加

---
+++ frontend/src/components/common/commonFormWindow/CommonFormWindow.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="wrapper-frame">
+    <div class="container">
+      <slot name="title"></slot>
+    </div>
+    <div class="container">
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  &-frame {
+    width: 80%;
+    max-width: 800px;
+    height: 60%;
+    max-height: 800px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    padding: 32px 16px;
+    border-radius: 24px;
+    box-shadow: 0px 8px 24px -2px rgba(0, 0, 0, 0.6);
+    display: flex;
+
+    > .container {
+      width: 50%;
+      height: 100%;
+      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+      overflow: auto;
+
+      &:first-child {
+        text-align: left;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 900px) {
+  .wrapper {
+    &-frame {
+      height: max-content;
+      flex-direction: column;
+
+      > .container {
+        width: 100%;
+        height: max-content;
+      }
+    }
+  }
+}
+</style>
