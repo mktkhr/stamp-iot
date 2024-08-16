af32d5c9b0b5071bf4c642fe03e98127a0443e0a
EMS engineering <j39g8bxh@gmail.com>
2024-08-17 00:25:17 +0900

    [update] #162 type指定できるように修正

--- frontend/src/components/common/commonButton/CommonButton.vue
+++ frontend/src/components/common/commonButton/CommonButton.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 interface Props {
   buttonTitle?: string;
   height?: string;
   width?: string;
-  backgroundColor?: string;
+  type?: 'fill' | 'text' | 'float' | 'outlined';
+  color?: string;
   titleColor?: string;
   icon?: string; // icomoonのアイコンを指定する
   fontSize?: string;
@@ -17,13 +20,13 @@ interface Props {
     | 'space-evenly';
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   buttonTitle: '',
-  height: '30px',
-  width: '60px',
-  backgroundColor: '#00000022',
-  titleColor: '#333',
-  fontSize: '12px',
+  height: 'max-content',
+  width: 'max-content',
+  type: 'float',
+  color: 'var(--ems-theme)',
+  fontSize: '14px',
   fontWeight: 'normal',
   justifyContent: 'center',
 });
@@ -34,13 +37,48 @@ interface Emits {
 
 const emit = defineEmits<Emits>();
 
-const clickButton = () => {
-  emit('clickButton');
+// 初期値
+const border = ref('');
+const backgroundColor = ref('');
+const textColor = ref('');
+const shadow = ref('');
+
+/**
+ * ボタンのデザインを算出する
+ */
+const calcalateDesign = () => {
+  switch (props.type) {
+    case 'fill':
+      border.value = 'none';
+      backgroundColor.value = props.color;
+      textColor.value = props.titleColor ?? 'white';
+      shadow.value = 'none';
+      break;
+    case 'float':
+      border.value = 'none';
+      backgroundColor.value = 'white';
+      textColor.value = props.titleColor ?? props.color;
+      shadow.value = '0 2px 8px #0006';
+      break;
+    case 'text':
+      border.value = 'none';
+      backgroundColor.value = 'white';
+      textColor.value = props.titleColor ?? props.color;
+      shadow.value = 'none';
+      break;
+    case 'outlined':
+      border.value = `1px solid ${props.color}`;
+      backgroundColor.value = 'white';
+      textColor.value = props.titleColor ?? props.color;
+      shadow.value = 'none';
+      break;
+  }
 };
+calcalateDesign();
 </script>
 
 <template>
-  <button class="button common-effect-ripple" @click="clickButton">
+  <button class="button common-effect-ripple" @click="emit('clickButton')">
     <slot v-if="icon" name="icon" :class="icon">
       <span :class="icon" class="common-icon-style icon-small"></span>
     </slot>
@@ -51,36 +89,43 @@ const clickButton = () => {
 <style lang="scss" scoped>
 $button_height: v-bind(height);
 $button_width: v-bind(width);
+$button_border: v-bind(border);
+$button_shodow: v-bind(shadow);
 $background_color: v-bind(backgroundColor);
 $justify_content: v-bind(justifyContent);
 $font_size: v-bind(fontSize);
 $font_weight: v-bind(fontWeight);
-$text_color: v-bind(titleColor);
+$text_color: v-bind(textColor);
 
 .button {
   height: $button_height;
   width: $button_width;
   background-color: $background_color;
   border-radius: 5px;
-  border: none;
+  border: $button_border;
   display: flex;
   align-items: center;
   justify-content: $justify_content;
   padding: 8px 16px;
   gap: 8px;
   pointer-events: all;
+  transition: all 0.2s ease;
+  box-shadow: $button_shodow;
   cursor: pointer;
+
   &-title {
     font-size: $font_size;
     font-weight: $font_weight;
     letter-spacing: 2px;
     color: $text_color;
   }
+
   &:hover {
-    filter: brightness(1.2);
+    filter: brightness(0.9);
   }
+
   &:active {
-    filter: brightness(1.2);
+    filter: brightness(0.9);
   }
 }
 
