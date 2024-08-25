f8f4ae34f8c94d3a2cbe322b8b0f8c12ce83a6e1
EMS engineering <j39g8bxh@gmail.com>
2024-08-25 19:53:50 +0900

    [add] #162 編集キャンセルダイアログを追加

---
+++ frontend/src/views/microController/detail/components/MicroControllerDetailEditCancelDialog.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import CommonButton from '@/components/common/commonButton/CommonButton.vue';
+import CommonDialog from '@/components/common/commonDialog/CommonDialog.vue';
+import CommonOverlay from '@/components/common/commonOverlay/CommonOverlay.vue';
+
+const emit = defineEmits<{
+  (e: 'onClickDeny'): void;
+  (e: 'onClickAccept'): void;
+}>();
+
+const modelValue = defineModel<boolean>({ required: true });
+</script>
+<template>
+  <CommonOverlay v-model="modelValue">
+    <CommonDialog
+      :title="$t('Dialog.cancelConfirm')"
+      :description="$t('Dialog.cancelConfirmDescription')"
+    >
+      <template #button>
+        <CommonButton
+          :button-title="$t('Button.no')"
+          width="80"
+          @click-button="emit('onClickDeny')"
+        />
+        <CommonButton :button-title="$t('Button.yes')" @click-button="emit('onClickAccept')" />
+      </template>
+    </CommonDialog>
+  </CommonOverlay>
+</template>
+<style lang="scss" scoped></style>
