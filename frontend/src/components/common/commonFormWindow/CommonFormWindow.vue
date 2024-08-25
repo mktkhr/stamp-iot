4894d9ea90769cdd738238b6b71f2e13be3b052f
EMS engineering <j39g8bxh@gmail.com>
2024-08-25 15:23:57 +0900

    [update] #162 propsでstyleを固定できるように修正

--- frontend/src/components/common/commonFormWindow/CommonFormWindow.vue
+++ frontend/src/components/common/commonFormWindow/CommonFormWindow.vue
@@ -1,4 +1,65 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+interface Props {
+  windowStyle: 'auto' | 'vertical' | 'horizontal';
+}
+
+const props = withDefaults(defineProps<Props>(), { windowStyle: 'auto' });
+
+// 固定値
+const MAX_CONTENT = 'max-content';
+const _100_WIDTH = '100%';
+const _50_WIDTH = '50%';
+const FLEX_ROW = 'row';
+const FLEX_COLUMN = 'column';
+
+const flexDirection = ref(FLEX_COLUMN);
+const containerWidth = ref(_100_WIDTH);
+const containerHeight = ref(MAX_CONTENT);
+
+onMounted(() => {
+  /**
+   * 画面サイズでstyleを再計算する
+   * propsでwindowのすたいるを固定できるように
+   */
+  const calculateStyle = () => {
+    switch (props.windowStyle) {
+      case 'auto':
+        {
+          const windowWidth = window.innerWidth;
+          if (windowWidth < 900) {
+            flexDirection.value = FLEX_COLUMN;
+            containerWidth.value = _100_WIDTH;
+            containerHeight.value = MAX_CONTENT;
+            break;
+          }
+          flexDirection.value = FLEX_ROW;
+          containerWidth.value = _50_WIDTH;
+          containerHeight.value = MAX_CONTENT;
+        }
+        break;
+      case 'horizontal':
+        {
+          flexDirection.value = FLEX_ROW;
+          containerWidth.value = _50_WIDTH;
+          containerHeight.value = MAX_CONTENT;
+        }
+        break;
+      case 'vertical':
+        {
+          flexDirection.value = FLEX_COLUMN;
+          containerWidth.value = _100_WIDTH;
+          containerHeight.value = MAX_CONTENT;
+        }
+        break;
+    }
+  };
+
+  calculateStyle();
+  window.addEventListener('resize', () => calculateStyle());
+});
+</script>
 
 <template>
   <div class="wrapper-frame">
@@ -28,11 +89,12 @@
     border-radius: 24px;
     box-shadow: 0px 8px 24px -2px rgba(0, 0, 0, 0.6);
     background-color: white;
+    flex-direction: v-bind(flexDirection);
     display: flex;
 
     > .container {
-      width: 50%;
-      height: 100%;
+      width: v-bind(containerWidth);
+      height: v-bind(containerHeight);
       padding: 16px;
       display: flex;
       flex-direction: column;
@@ -41,19 +103,6 @@
 
       &:first-child {
         text-align: left;
-      }
-    }
-  }
-}
-
-@media screen and (max-width: 900px) {
-  .wrapper {
-    &-frame {
-      flex-direction: column;
-
-      > .container {
-        width: 100%;
-        height: max-content;
       }
     }
   }
