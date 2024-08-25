93c50d865f071212d32119b95878504f22f88224
EMS engineering <j39g8bxh@gmail.com>
2024-08-25 23:09:39 +0900

    [update] #162 サンプルテストを修正

--- frontend/tests/unit/example.spec.ts
+++ frontend/tests/unit/example.spec.ts
@@ -1,10 +1,10 @@
+import SidebarTile from '@/components/common/commonSidebar/components/sidebarTile/SidebarTile.vue';
 import { shallowMount } from '@vue/test-utils';
-import FormWindow from '@/components/common/FormWindow.vue';
 
 describe('App.vue', () => {
   it('renders props.title when passed', () => {
     const title = 'new message';
-    const wrapper = shallowMount(FormWindow, {
+    const wrapper = shallowMount(SidebarTile, {
       props: { title: title },
     });
     expect(wrapper.text()).toMatch(title);
