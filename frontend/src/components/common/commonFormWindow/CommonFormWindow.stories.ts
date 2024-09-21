4abb38587b7b81d766a2a2e1222ce638be2304b5
EMS engineering <j39g8bxh@gmail.com>
2024-09-22 00:24:28 +0900

    [add] #173 CommonFormWindowのStoryを追加

---
+++ frontend/src/components/common/commonFormWindow/CommonFormWindow.stories.ts
@@ -0,0 +1,53 @@
+import { Meta, StoryObj } from '@storybook/vue3';
+import CommonButton from '../commonButton/CommonButton.vue';
+import CommonFormWindow from './CommonFormWindow.vue';
+
+const meta: Meta<typeof CommonFormWindow> = {
+  title: 'Common/CommonFormWindow',
+  component: CommonFormWindow,
+  render: (args) => ({
+    components: { CommonFormWindow, CommonButton },
+    setup() {
+      return { args };
+    },
+    template: `
+      <CommonFormWindow v-bind="args">
+        <template v-slot:title>
+          <span>タイトルスロット</span>
+        </template>
+        <template v-slot:content>
+          <CommonButton button-title="コンテンツスロット" />
+        </template>
+      </CommonFormWindow
+    `,
+  }),
+  // Props
+  argTypes: {
+    windowStyle: { options: ['auto', 'vertical', 'horizontal'] },
+  },
+  args: {
+    title: 'タイトル',
+    content: 'コンテンツ',
+  },
+  parameters: {
+    slots: {
+      title: {
+        description: 'Totle slot',
+        template: `<span>タイトルスロット</span>`,
+      },
+      content: {
+        description: 'Content slot',
+        components: { CommonButton },
+        template: `<CommonButton button-title="コンテンツスロット" />`,
+      },
+    },
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
