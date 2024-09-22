a24f9abcc426ab49cb182871baaaaac3e92c5252
EMS engineering <j39g8bxh@gmail.com>
2024-09-22 22:34:12 +0900

    [add] #173 CommonMenuのStoryを追加

---
+++ frontend/src/components/common/commonMenu/CommonMenu.stories.ts
@@ -0,0 +1,57 @@
+import { Meta, StoryObj } from '@storybook/vue3';
+import { onMounted, ref } from 'vue';
+import CommonButton from '../commonButton/CommonButton.vue';
+import CommonDialog from '../commonDialog/CommonDialog.vue';
+import CommonMenu from './CommonMenu.vue';
+
+const meta: Meta<typeof CommonMenu> = {
+  title: 'Common/CommonMenu',
+  component: CommonMenu,
+  render: (args) => ({
+    components: {
+      CommonMenu,
+      CommonButton,
+      CommonDialog,
+    },
+    setup() {
+      // レンダリングを待つ必要があるので，Storybook用にフラグを持たせる
+      const isMounted = ref(false);
+      onMounted(() => {
+        isMounted.value = true;
+      });
+      return { args, isMounted };
+    },
+    template: `
+      <CommonMenu v-if="isMounted" v-bind="args">
+        <CommonDialog
+          title="コンテンツタイトル"
+          description="コンテンツ内容"
+        >
+          <CommonButton button-title="コンテンツスロット" />
+        </CommonDialog>
+        <template v-slot:activator>
+          <CommonButton button-title="アクティベータースロット" />
+        </template>
+      </CommonMenu>
+    `,
+  }),
+  decorators: [
+    () => ({ template: '<div id="app" style="height: 100vh; width: 100vw;"><story/></div>' }),
+  ],
+  argTypes: {
+    teleportDestination: { control: 'text' },
+    persistent: { control: 'boolean' },
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: { teleportDestination: '#app', persistent: false },
+};
+
+export const Persistent: Story = {
+  args: { teleportDestination: '#app', persistent: true },
+};
