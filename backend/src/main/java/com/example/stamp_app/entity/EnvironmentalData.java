056bf3505ace24075bf3046724f14a44dfb637d1
EMS engineering <j39g8bxh@gmail.com>
2024-11-02 21:06:52 +0900

    [update] #167 測定値関連Entityの@Dataを置換

--- backend/src/main/java/com/example/stamp_app/entity/EnvironmentalData.java
+++ backend/src/main/java/com/example/stamp_app/entity/EnvironmentalData.java
@@ -1,15 +1,15 @@
 package com.example.stamp_app.entity;
 
-import com.example.stamp_app.controller.param.EnvironmentalDataParam;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.Data;
-
-import java.util.ArrayList;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Data
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EnvironmentalData {
 
     @Id
@@ -38,46 +38,4 @@ public class EnvironmentalData {
     @ManyToOne
     private MeasuredDataMaster measuredDataMaster;
 
-
-    /**
-     * environmentalParamからEnvironmentalDataを作成する
-     *
-     * @param environmentalDataParam コントローラーが受け取ったパラメータ
-     * @param measuredDataMaster 測定データマスター
-     * @return environmentalData
-     */
-    public static EnvironmentalData createEnvironmentalData(EnvironmentalDataParam environmentalDataParam, MeasuredDataMaster measuredDataMaster){
-
-        var environmentalData = new EnvironmentalData();
-        environmentalData.setAirPress(environmentalDataParam.getAirPress());
-        environmentalData.setTemp(environmentalDataParam.getTemp());
-        environmentalData.setHumi(environmentalDataParam.getHumi());
-        environmentalData.setCo2Concent(environmentalDataParam.getCo2Concent());
-        environmentalData.setTvoc(environmentalDataParam.getTvoc());
-        environmentalData.setAnalogValue(environmentalDataParam.getAnalogValue());
-        environmentalData.setMeasuredDataMaster(measuredDataMaster);
-
-        return environmentalData;
-
-    }
-
-    /**
-     * EnvironmentalParamのリストからEnvironmentalDataのリストを作成する
-     *
-     * @param environmentalDataParamList EnvironmentalDataのリスト
-     * @param measuredDataMaster 測定データマスター
-     * @return environmentalDataList
-     */
-    public static List<EnvironmentalData> createEnvironmentalDataList(List<EnvironmentalDataParam> environmentalDataParamList, MeasuredDataMaster measuredDataMaster){
-
-        var environmentalDataList = new ArrayList<EnvironmentalData>();
-
-        for(var environmentalDataParam: environmentalDataParamList){
-            var environmentalData = EnvironmentalData.createEnvironmentalData(environmentalDataParam, measuredDataMaster);
-            environmentalDataList.add(environmentalData);
-        }
-
-        return environmentalDataList;
-
-    }
 }
