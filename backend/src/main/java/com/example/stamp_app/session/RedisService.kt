b7a4678b4152baf1c2c8c17906ad1e562a4d7958
EMS engineering <j39g8bxh@gmail.com>
2024-12-01 22:57:05 +0900

    [update] #188 RedisServiceをKotlinに変換

---
+++ backend/src/main/java/com/example/stamp_app/session/RedisService.kt
@@ -0,0 +1,43 @@
+package com.example.stamp_app.session
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
+
+@Service
+class RedisService(
+		private val redisTemplate: RedisTemplate<String, Any>
+) {
+
+	/**
+	 * セッション情報の削除
+	 *
+	 * @param key キー
+	 */
+	fun delete(key: String?) {
+		if (key != null) {
+			redisTemplate.delete(key)
+		}
+	}
+
+	/**
+	 * セッション情報の取得
+	 *
+	 * @param key セッションUUID
+	 * @return String ユーザーUUID or null
+	 */
+	@Throws(IllegalArgumentException::class)
+	fun getUserUuidFromSessionUuid(key: String?): String {
+		return redisTemplate.opsForValue()[key] as String
+	}
+
+	/**
+	 * セッション情報の保存
+	 *
+	 * @param key   セッションUUID
+	 * @param value ユーザーUUID
+	 */
+	operator fun set(key: String?, value: String, timeInSec: Long) {
+		redisTemplate.opsForValue()[key, value, timeInSec] = TimeUnit.SECONDS
+	}
+}
