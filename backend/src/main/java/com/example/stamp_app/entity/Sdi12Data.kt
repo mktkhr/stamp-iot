0bae5ae59d1c460b56d3ed1a4f959ba97bce4cb9
EMS engineering <j39g8bxh@gmail.com>
2024-12-01 16:02:53 +0900

    [update] #188 SDI-12データEntityをKotlinに変換

---
+++ backend/src/main/java/com/example/stamp_app/entity/Sdi12Data.kt
@@ -0,0 +1,49 @@
+package com.example.stamp_app.entity
+
+import com.fasterxml.jackson.annotation.JsonBackReference
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotNull
+import lombok.AllArgsConstructor
+import lombok.Getter
+import lombok.NoArgsConstructor
+
+@Entity
+data class Sdi12Data (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column
+    val sdiAddress: @NotNull String? = null,
+
+    @Column(name = "volumetricWaterContent")
+    val vwc: String? = null,
+
+    @Column(name = "soilTemperature")
+    val soilTemp: String? = null,
+
+    @Column(name = "bulkRelativePermittivity")
+    val brp: String? = null,
+
+    @Column(name = "soilBulkElectricConductivity")
+    val sbec: String? = null,
+
+    @Column(name = "soilPoreWaterElectricConductivity")
+    val spwec: String? = null,
+
+    @Column(name = "gravitationalAccelerationXAxis")
+    val gax: String? = null,
+
+    @Column(name = "gravitationalAccelerationYAxis")
+    val gay: String? = null,
+
+    @Column(name = "gravitationalAccelerationZAxis")
+    val gaz: String? = null,
+
+    @JsonBackReference
+    @ManyToOne
+    val measuredDataMaster: MeasuredDataMaster? = null,
+
+    @ManyToOne
+    val sensor: Sensor? = null
+)
