378465e31274af51986a22ede2ea57daaa43fac7
EMS engineering <j39g8bxh@gmail.com>
2024-12-01 22:26:57 +0900

    [update] #188 RedisConfigをKotlinに変換

---
+++ backend/src/main/java/com/example/stamp_app/session/RedisConfig.kt
@@ -0,0 +1,23 @@
+package com.example.stamp_app.session
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+    @Bean
+    fun redisTemplate(factory: RedisConnectionFactory?): RedisTemplate<String, Any> {
+        val template = RedisTemplate<String, Any>()
+        val stringRedisSerializer = StringRedisSerializer()
+
+        template.connectionFactory = factory
+        template.keySerializer = stringRedisSerializer
+        template.hashKeySerializer = stringRedisSerializer
+        template.afterPropertiesSet()
+
+        return template
+    }
+}
