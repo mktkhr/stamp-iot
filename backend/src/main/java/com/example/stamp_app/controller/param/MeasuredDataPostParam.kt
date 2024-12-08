246ea1056cc7c0c0b66242eaa297b1959ad7414e
EMS engineering <j39g8bxh@gmail.com>
2024-12-08 23:19:18 +0900

    [update] #188 MeasuredDataPostParamをKotlinに変換

---
+++ backend/src/main/java/com/example/stamp_app/controller/param/MeasuredDataPostParam.kt
@@ -0,0 +1,22 @@
+package com.example.stamp_app.controller.param
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+
+@Schema(description = "測定結果登録パラメータ")
+data class MeasuredDataPostParam(
+
+	@Schema(description = "MACアドレス", example = "AA:AA:AA:AA:AA:AA")
+	val macAddress: @NotBlank @Pattern(regexp = "^([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}$") String,
+
+	@Schema(description = "SDI-12パラメータリスト")
+	val sdi12Param: List<Sdi12Param>,
+
+	@Schema(description = "環境データパラメータリスト")
+	val environmentalDataParam: List<EnvironmentalDataParam>,
+
+	@Schema(description = "電圧", example = "11.11")
+	val voltage: String
+
+)
