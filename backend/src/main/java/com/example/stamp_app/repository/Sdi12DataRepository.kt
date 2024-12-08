0516a0256502b5132a236a2a1b51a7faaec4fd13
EMS engineering <j39g8bxh@gmail.com>
2024-12-08 23:17:11 +0900

    [update] #188 フォーマットを実行

--- backend/src/main/java/com/example/stamp_app/repository/Sdi12DataRepository.kt
+++ backend/src/main/java/com/example/stamp_app/repository/Sdi12DataRepository.kt
@@ -8,19 +8,19 @@ import java.util.*
 
 interface Sdi12DataRepository : JpaRepository<Sdi12Data?, UUID?> {
 
-    @Query(value = "SELECT sdi_address " +
-            "FROM sdi12data " +
-            "LEFT JOIN measured_data_master " +
-            "ON sdi12data.measured_data_master_id = measured_data_master.id " +
-            "LEFT JOIN micro_controller " +
-            "ON measured_data_master.micro_controller_id = micro_controller.id " +
-            "LEFT JOIN account " +
-            "ON micro_controller.account_id = account.id " +
-            "WHERE account.uuid = :#{#userUuid} " +
-            "AND micro_controller.id = :#{#microControllerId} " +
-            "GROUP BY sdi12data.sdi_address", nativeQuery = true)
-    fun findSdiAddressGroupBySdiAddress(@Param("userUuid") userUuid: UUID?, @Param("microControllerId") microControllerId: Long?): List<String?>?
+	@Query(value = "SELECT sdi_address " +
+		"FROM sdi12data " +
+		"LEFT JOIN measured_data_master " +
+		"ON sdi12data.measured_data_master_id = measured_data_master.id " +
+		"LEFT JOIN micro_controller " +
+		"ON measured_data_master.micro_controller_id = micro_controller.id " +
+		"LEFT JOIN account " +
+		"ON micro_controller.account_id = account.id " +
+		"WHERE account.uuid = :#{#userUuid} " +
+		"AND micro_controller.id = :#{#microControllerId} " +
+		"GROUP BY sdi12data.sdi_address", nativeQuery = true)
+	fun findSdiAddressGroupBySdiAddress(@Param("userUuid") userUuid: UUID?, @Param("microControllerId") microControllerId: Long?): List<String>?
 
-    fun findBySdiAddress(sdiAddress: String?): List<Sdi12Data?>?
+	fun findBySdiAddress(sdiAddress: String?): List<Sdi12Data>?
 
 }
