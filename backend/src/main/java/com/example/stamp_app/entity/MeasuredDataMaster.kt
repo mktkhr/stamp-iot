37c24264a1c259e89e20935658e808285bb7cb18
EMS engineering <j39g8bxh@gmail.com>
2024-12-08 23:01:36 +0900

    [update] #188 フォーマットを実行

--- backend/src/main/java/com/example/stamp_app/entity/MeasuredDataMaster.kt
+++ backend/src/main/java/com/example/stamp_app/entity/MeasuredDataMaster.kt
@@ -2,40 +2,37 @@ package com.example.stamp_app.entity
 
 import com.fasterxml.jackson.annotation.JsonManagedReference
 import jakarta.persistence.*
-import lombok.AllArgsConstructor
-import lombok.Getter
-import lombok.NoArgsConstructor
 import java.time.LocalDateTime
 
 @Entity
 data class MeasuredDataMaster(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	val id: Long? = null,
 
-    @Column
-    val dayOfYear: String? = null,
+	@Column
+	val dayOfYear: String? = null,
 
-    @Column
-    val createdAt: LocalDateTime? = null,
+	@Column
+	val createdAt: LocalDateTime? = null,
 
-    @Column
-    val updatedAt: LocalDateTime? = null,
+	@Column
+	val updatedAt: LocalDateTime? = null,
 
-    @Column
-    val deletedAt: LocalDateTime? = null,
+	@Column
+	val deletedAt: LocalDateTime? = null,
 
 	@ManyToOne
 	val microController: MicroController,
 
-    @JsonManagedReference
-    @OneToMany(mappedBy = "measuredDataMaster")
-    val sdi12Data: List<Sdi12Data>? = null,
+	@JsonManagedReference
+	@OneToMany(mappedBy = "measuredDataMaster")
+	val sdi12Data: List<Sdi12Data>? = null,
 
-    @JsonManagedReference
-    @OneToMany(mappedBy = "measuredDataMaster")
-    val environmentalData: List<EnvironmentalData>? = null,
+	@JsonManagedReference
+	@OneToMany(mappedBy = "measuredDataMaster")
+	val environmentalData: List<EnvironmentalData>? = null,
 
-    @Column
-    val voltage: String? = null,
+	@Column
+	val voltage: String? = null,
 )
