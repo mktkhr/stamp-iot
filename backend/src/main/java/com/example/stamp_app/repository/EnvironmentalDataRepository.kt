310b4e8af1b11e9745e27c7c9d475299919bb7e1
EMS engineering <j39g8bxh@gmail.com>
2024-12-08 23:06:33 +0900

    [update] #188 notnullに修正

--- backend/src/main/java/com/example/stamp_app/repository/EnvironmentalDataRepository.kt
+++ backend/src/main/java/com/example/stamp_app/repository/EnvironmentalDataRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
 interface EnvironmentalDataRepository : JpaRepository<EnvironmentalData?, UUID?> {
-    
-    override fun findAll(): List<EnvironmentalData?>
+
+	override fun findAll(): List<EnvironmentalData>
 
 }
