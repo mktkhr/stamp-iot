8cbe3bbad530852050e0a43e7dc818f47fa2af92
EMS engineering <j39g8bxh@gmail.com>
2024-12-27 18:24:49 +0900

    [add] #188 finalクラスをmockできるようにinlineパッケージを追加

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 	testImplementation("org.mockito:mockito-core:5.4.0")
 	testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+	testImplementation("org.mockito.kotlin:mockito-inline:5.2.0")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
