4e330c6361ab384d3d57db910b55df50e9a71555
EMS engineering <j39g8bxh@gmail.com>
2025-01-01 13:45:39 +0900

    [update] #188 dataクラス用のアノテーションに変更し，バリデーションエラーメッセージを追加

--- backend/src/main/java/com/example/stamp_app/controller/param/microController/MicroControllerPatchParam.kt
+++ backend/src/main/java/com/example/stamp_app/controller/param/microController/MicroControllerPatchParam.kt
@@ -4,22 +4,27 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.annotation.Nullable
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
-import org.springframework.validation.annotation.Validated
 import java.util.*
 
 @Schema(description = "マイコン更新データ")
-@Validated
 data class MicroControllerPatchParam(
 	@Schema(description = "マイコンUUID", example = "61d5f7a7-7629-496e-be68-cfe022264578")
-	val microControllerUuid: @NotNull UUID,
+	@field:NotNull(message = "UUID must not be NULL")
+	val microControllerUuid: UUID,
 
 	@Schema(description = "マイコン名", example = "サンプル端末")
 	val name: String,
 
 	@Schema(description = "測定間隔", example = "60")
-	val interval: @NotNull @Pattern(regexp = "^(60|30|20|15|10|5|1)$") String,
+	@field:NotNull(message = "Interval must not be NULL")
+	@field:Pattern(regexp = "^(60|30|20|15|10|5|1)$")
+	val interval: String,
 
 	@Nullable
 	@Schema(description = "SDI-12アドレス", example = "1,3")
-	val sdi12Address: @Pattern(regexp = "^((([0-9A-Za-z],)+[0-9A-za-z])|([0-9A-za-z])?)$") String? = null
+	@field:Pattern(
+		regexp = "^((([0-9A-Za-z],)+[0-9A-Za-z])|([0-9A-Za-z])?)$",
+		message = "SDI-12 address pattern is invalid"
+	)
+	val sdi12Address: String? = null
 )
