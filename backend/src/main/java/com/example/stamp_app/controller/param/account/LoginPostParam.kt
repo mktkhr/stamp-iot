6a0f1e09c08ae3e41dcbb02d18737a0c0a72e95a
EMS engineering <j39g8bxh@gmail.com>
2025-01-01 14:37:00 +0900

    [update] #188 dataクラス用のアノテーションに変更し，バリデーションエラーメッセージを追加

--- backend/src/main/java/com/example/stamp_app/controller/param/account/LoginPostParam.kt
+++ backend/src/main/java/com/example/stamp_app/controller/param/account/LoginPostParam.kt
@@ -6,10 +6,16 @@ import jakarta.validation.constraints.Pattern
 
 @Schema(description = "ログインパラメータ")
 data class LoginPostParam(
+	@field:NotBlank(message = "Email must not be NULL")
+	@field:Pattern(
+		regexp = "^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$",
+		message = "Email pattern is invalid"
+	)
 	@Schema(description = "メールアドレス", example = "aaa@example.com")
-	val email: @NotBlank @Pattern(regexp = "^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$") String,
+	val email: String,
 
 	// NOTE: ログイン時のパスワードはパターン推測を回避するため，パターンバリデーションを行わない
+	@field:NotBlank(message = "Password must not be NULL")
 	@Schema(description = "パスワード", example = "SamplePassword")
-	val password: @NotBlank String
+	val password: String
 )
