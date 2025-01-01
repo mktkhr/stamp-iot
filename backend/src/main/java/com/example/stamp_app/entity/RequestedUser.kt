b076234fb159fad62eaa1cca65fa6bece2ef3e08
EMS engineering <j39g8bxh@gmail.com>
2025-01-01 13:49:35 +0900

    [update] #188 必須のフィールドに変更

--- backend/src/main/java/com/example/stamp_app/entity/RequestedUser.kt
+++ backend/src/main/java/com/example/stamp_app/entity/RequestedUser.kt
@@ -1,10 +1,9 @@
 package com.example.stamp_app.entity
 
-import lombok.Data
 import org.springframework.stereotype.Component
 
-@Component // NOTE: Interceptorでsetするため，@Dataを使用する
-data class RequestedUser (
-    var sessionUuid: String? = null,
-    var userUuid: String? = null
+@Component
+data class RequestedUser(
+	var sessionUuid: String? = null,
+	var userUuid: String
 )
