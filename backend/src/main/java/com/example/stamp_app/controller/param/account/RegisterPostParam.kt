a29f448a5fc58974f3d0e255019088cdc1839e65
EMS engineering <j39g8bxh@gmail.com>
2025-01-01 14:42:42 +0900

    [update] #188 dataクラス用のアノテーションに変更し，バリデーションエラーメッセージを追加

--- backend/src/main/java/com/example/stamp_app/controller/param/account/RegisterPostParam.kt
+++ backend/src/main/java/com/example/stamp_app/controller/param/account/RegisterPostParam.kt
@@ -6,9 +6,16 @@ import jakarta.validation.constraints.Pattern
 
 @Schema(description = "アカウント登録パラメータ")
 data class RegisterPostParam(
+	@field:NotNull(message = "Email must not be NULL")
+	@field:Pattern(
+		regexp = "^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$",
+		message = "Email pattern is invalid"
+	)
 	@Schema(description = "メールアドレス", example = "aaa@example.com")
-	val email: @NotNull @Pattern(regexp = "^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$") String,
+	val email: String,
 
+	@field:NotNull(message = "Password must not be NULL")
+	@field:Pattern(regexp = "^(?=.*[A-Z])[a-zA-Z0-9.?/-]{8,24}$", message = "Password pattern must not be NULL")
 	@Schema(description = "パスワード", example = "SamplePassword")
-	val password: @NotNull @Pattern(regexp = "^(?=.*[A-Z])[a-zA-Z0-9.?/-]{8,24}$") String
+	val password: String
 )
