0830a945ff38c3885b612954899065c9a9c7d321
EMS engineering <j39g8bxh@gmail.com>
2025-02-10 02:57:43 +0900

    [fix] #207 論理削除済みアカウントの場合もエラーになるように修正

--- backend/src/main/java/com/example/stamp_app/service/AccountService.kt
+++ backend/src/main/java/com/example/stamp_app/service/AccountService.kt
@@ -97,7 +97,7 @@ class AccountService(
 		val account = accountRepository.findByUuid(UUID.fromString(userUuid))
 
 		// アカウントが存在しない場合，400を返す
-		if (account == null) {
+		if (account == null || account.deletedAt != null) {
 			log.error("This account does not exist.")
 			throw IllegalArgumentException()
 		}
