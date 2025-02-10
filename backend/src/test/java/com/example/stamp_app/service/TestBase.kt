0c2fd48192f4da8bb528e6131f640c1c6228c9a2
EMS engineering <j39g8bxh@gmail.com>
2025-02-10 23:59:01 +0900

    [update] #207 SpringのTransactionalでrollbackされるように修正

--- backend/src/test/java/com/example/stamp_app/service/TestBase.kt
+++ backend/src/test/java/com/example/stamp_app/service/TestBase.kt
@@ -13,13 +13,24 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener
 import java.io.File
 import java.util.concurrent.atomic.AtomicBoolean
 import javax.sql.DataSource
 
 @SpringBootTest
 @ActiveProfiles("unit-test")
+@TestExecutionListeners(
+	value = [
+		DependencyInjectionTestExecutionListener::class,
+		TransactionalTestExecutionListener::class
+	],
+	mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) // NOTE: 本クラスを継承したクラスを跨いでisMigratedが共有されるようにする
 abstract class TestBase {
 
@@ -55,7 +66,9 @@ abstract class TestBase {
 	 */
 	@BeforeEach
 	fun setupDbUnit() {
-		dbUnitConnection = DatabaseConnection(dataSource.connection, "PUBLIC") // publicスキーマを指定
+		val transactionAwareDataSource = TransactionAwareDataSourceProxy(dataSource) // Transactionに考慮したDataSourceを取得
+
+		dbUnitConnection = DatabaseConnection(transactionAwareDataSource.connection, "PUBLIC") // publicスキーマを指定
 			.apply {
 				config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, PostgresqlDataTypeFactory())
 			}
