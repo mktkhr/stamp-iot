8b9126e0f733e59aa5484ae13d74ef44bb7c2403
EMS engineering <j39g8bxh@gmail.com>
2025-02-10 23:55:54 +0900

    [clean] #207 不要なアノテーション

--- backend/src/test/java/com/example/stamp_app/service/account/AccountServiceTest.kt
+++ backend/src/test/java/com/example/stamp_app/service/account/AccountServiceTest.kt
@@ -15,13 +15,11 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.Rollback
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 
 @SpringBootTest
 @Transactional
-@Rollback
 class AccountServiceTest : TestBase() {
 
 	@Autowired
