70aedb0026382c8decbd8311e7a03389eff7bf36
EMS engineering <j39g8bxh@gmail.com>
2025-02-11 01:23:34 +0900

    [add] #207 MicroControllerServiceのマイコン詳細取得のテストを追加

--- backend/src/test/java/com/example/stamp_app/service/micro_controller/MicroControllerServiceTest.kt
+++ backend/src/test/java/com/example/stamp_app/service/micro_controller/MicroControllerServiceTest.kt
@@ -3,17 +3,23 @@ package com.example.stamp_app.service.micro_controller
 import com.example.stamp_app.controller.response.MicroControllerGetResponse
 import com.example.stamp_app.domain.exception.EMSResourceDuplicationException
 import com.example.stamp_app.domain.exception.EMSResourceNotFoundException
+import com.example.stamp_app.entity.MicroController
+import com.example.stamp_app.entity.RequestedUser
 import com.example.stamp_app.service.MicroControllerService
 import com.example.stamp_app.service.TestBase
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
+import java.util.*
 
 @Transactional
 class MicroControllerServiceTest : TestBase() {
 	@Autowired
 	private lateinit var microControllerService: MicroControllerService
+
+	@Autowired
+	protected lateinit var requestedUser: RequestedUser
 
 	@Nested
 	inner class AddMicroControllerRelationTest {
@@ -128,6 +134,84 @@ class MicroControllerServiceTest : TestBase() {
 			)
 
 			Assertions.assertEquals(expectedList, microControllerList)
+		}
+
+	}
+
+	@Nested
+	inner class GetMicroControllerDetailTest {
+
+		@Test
+		@DisplayName("UUIDのフォーマットで指定されていない場合，IllegalArgumentExceptionが発生すること")
+		fun `case3-1`() {
+			assertThrows<IllegalArgumentException> {
+				microControllerService.getMicroControllerDetail("1")
+			}
+		}
+
+		@Test
+		@DisplayName("存在しないUUIDを指定した場合，EMSResourceNotFoundExceptionが発生すること")
+		fun `case3-2`() {
+			loadDatasetAndInsert("src/test/resources/service/micro_controller/case3.xml")
+
+			assertThrows<EMSResourceNotFoundException> {
+				microControllerService.getMicroControllerDetail("99999999-043d-4117-8a24-771351bce045")
+			}
+		}
+
+		@Test
+		@DisplayName("アカウントの紐付けが存在しないMicroControllerのUUIDを指定した場合，EMSResourceNotFoundExceptionが発生すること")
+		fun `case3-3`() {
+			loadDatasetAndInsert("src/test/resources/service/micro_controller/case3.xml")
+
+			assertThrows<EMSResourceNotFoundException> {
+				microControllerService.getMicroControllerDetail("2ea20e98-043d-4117-8a24-771351bce045")
+			}
+		}
+
+		@Test
+		@DisplayName("MicroControllerの所有者とリクエストユーザーが異なる場合，EMSResourceNotFoundExceptionが発生すること")
+		fun `case3-4`() {
+
+			requestedUser.userUuid = "11111111-043d-4117-8a24-771351bce045"
+
+			loadDatasetAndInsert("src/test/resources/service/micro_controller/case3.xml")
+
+			assertThrows<EMSResourceNotFoundException> {
+				microControllerService.getMicroControllerDetail("1ea20e98-043d-4117-8a24-771351bce045")
+			}
+		}
+
+		@Test
+		@DisplayName("MicroControllerの所有者とリクエストユーザーが一致する場合，正常に取得できること")
+		fun `case3-5`() {
+
+			requestedUser.userUuid = "8ea20e98-043d-4117-8a24-771351bce045"
+
+			loadDatasetAndInsert("src/test/resources/service/micro_controller/case3.xml")
+
+			val microControllerDetail =
+				microControllerService.getMicroControllerDetail("1ea20e98-043d-4117-8a24-771351bce045")
+
+			val expectedMicroController = MicroController(
+				1,
+				UUID.fromString("1ea20e98-043d-4117-8a24-771351bce045"),
+				"テスト端末",
+				"AA:AA:AA:AA:AA:AA",
+				"60",
+				"1",
+				LocalDateTime.parse("2025-01-01T01:00:00.000"),
+				LocalDateTime.parse("2025-01-01T01:00:00.000"),
+				null,
+				null,
+				null
+			)
+
+			// NOTE: accountとmeasuredMasterを含めるとStackOverflowが発生するため，nullに変更する
+			val ignoreAccountAndMeasuredDataMaster =
+				microControllerDetail.copy(account = null, measuredDataMasters = null)
+
+			Assertions.assertEquals(expectedMicroController, ignoreAccountAndMeasuredDataMaster)
 		}
 
 	}
